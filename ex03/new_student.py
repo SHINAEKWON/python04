7da411cbfbda6e554aae8ec4cb62698e50b520c2
Shin Ae KWON <tradekwonsa@gmail.com>
2025-07-28 07:00:38 +0200

    ex03 done

--- ex03/new_student.py
+++ ex03/new_student.py
@@ -2,10 +2,26 @@ import random
 import string
 from dataclasses import dataclass, field
 
+
 def generate_id() -> str:
-	return "".join(random.choices(string.ascii_lowercase, k = 15))
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
+
+def generate_login(name: str, surname: str) -> str:
+    return name[0] + surname
+
 
 @dataclass
 class Student:
-	#your code here
-	pass
+    """
+    Student Class
+    """
+    name: str
+    surname: str
+    active: bool = True
+    login: str = field(init=False)
+    id: str = field(init=False)
+
+    def __post_init__(self):
+        self.login = generate_login(self.name, self.surname)
+        self.id = generate_id()
--- ex03/tester.py
+++ ex03/tester.py
@@ -2,9 +2,12 @@ from new_student import Student
 
 
 def main():
-	student = Student(name = "Edward", surname = "agle")
-	print(student)
+    student = Student(name="Edward", surname="agle")
+    print(student)
+
+    student = Student(name="Edward", surname="agle", id="toto")
+    print(student)
 
 
 if __name__ == "__main__":
-	main()
+    main()
