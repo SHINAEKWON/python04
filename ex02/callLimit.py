c8ddd5f177e35b11e3be06986db2e366416fb2cf
Shin Ae KWON <tradekwonsa@gmail.com>
2025-07-28 05:59:54 +0200

    ex02 done

---
+++ ex02/callLimit.py
@@ -0,0 +1,14 @@
+
+def callLimit(limit: int):
+    count = 0
+
+    def callLimiter(function):
+        def limit_function(*args: any, **kwds: any):
+            nonlocal count
+            if count < limit:
+                function(*args, **kwds)
+                count += 1
+            else:
+                print("Error:", function, "call too many times")
+        return limit_function
+    return callLimiter
